b7292f5993b6f7bd8704cc54a0db114b1fb223bc
alchermd <johnalcherdoloiras@gmail.com>
2017-11-02 23:39:38 +0800

    Create database and migrate objects.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+# Main application instance.
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile("app.cfg")
 
+# Extension objects.
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+# Expose some parts of the application to be easily imported.
+from .models import db
 from . import views
