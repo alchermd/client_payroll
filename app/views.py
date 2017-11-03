605e3516e6c4965121c0860577f43c4aa0a8317a
alchermd <johnalcherdoloiras@gmail.com>
2017-11-03 19:46:50 +0800

    Protect dashboard route and only allow logged in users to access it. Closes #2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from . import app
 from .forms import LoginForm
-from flask import redirect, render_template, url_for
+from .models import Admin
+from flask import flash, redirect, render_template, session, url_for
+from functools import wraps
+from passlib.hash import pbkdf2_sha256
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("ADMIN") is None:
+            flash("Please login to continue", "info")
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route("/")
@@ -12,7 +25,20 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print("Login!")
-        return redirect(url_for("login"))
+        admin = Admin.query.filter_by(username=form.username.data).first()
+
+        if pbkdf2_sha256.verify(form.password.data, admin.password):
+            session["ADMIN"] = admin.username
+            return redirect(url_for("dashboard"))
+
+        else:
+            flash("Incorrect credentials.", "warning")
+            return redirect(url_for("login"))
 
     return render_template("login.html", form=form)
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return "welcome to dashboard!"
