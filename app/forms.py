f595fb0eef80fe319170904dc19715da645f0327
alchermd <johnalcherdoloiras@gmail.com>
2017-11-05 05:34:03 +0800

    Create UpdatePaymentForm.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
+from .models import Employee, Employer
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import IntegerField, PasswordField, SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
@@ -7,3 +8,15 @@ class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     login = SubmitField("Login")
+
+
+class UpdatePaymentForm(FlaskForm):
+    employer_choices = [ (employer.id, employer.name) for employer in Employer.query.all()]
+    employer = SelectField("Employer", choices=employer_choices, coerce=int)
+
+    employee_choices = [ (employee.id, employee.name) for employee in Employee.query.all()]
+    employee = SelectField("Employee", choices=employee_choices, coerce=int)
+
+    amount = IntegerField("Amount")
+
+    submit = SubmitField("Save")
